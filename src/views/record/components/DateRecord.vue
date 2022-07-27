1cfe42b616749062c94ce1f2fb51b196b5bec7fe
afanti <afanti@juexiaotime.com>
2022-07-27 15:01:14 +0800

    feat: 完成做题记录静态页面

---
+++ src/views/record/components/DateRecord.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="date-record mb-20">
+    <div class="title">
+      <img src="../../../assets/icons/date.png" />
+      <span>2022/06/20</span>
+    </div>
+    <div class="record-list">
+      <record-item
+        v-for="i in 7"
+        :key="i"
+        :total-scores="10"
+        :scores="2"
+        :total-questions="10"
+        :questions="2"
+        chapter-name="刑法的解释"
+        class="mt-20"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="less">
+.date-record {
+  .title {
+    height: 24px;
+    line-height: 24px;
+    img {
+      margin-right: 6px;
+    }
+    span {
+      font-weight: bold;
+    }
+  }
+  .record-list {
+    display: flex;
+    flex-wrap: wrap;
+    .record-item {
+      width: 280px;
+      height: 80px;
+      margin-right: 13px;
+    }
+    & .record-item:nth-child(4n) {
+      margin-right: 0;
+    }
+  }
+}
+</style>
---
+++ src/views/record/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="record mt-20">
+    <div class="record-title pt-20 pb-20 flex-bt">
+      <span>做题记录</span>
+      <router-link to="/">
+        <span>&nbsp;返回题库&nbsp;</span>
+      </router-link>
+    </div>
+    <DateRecord />
+    <DateRecord />
+    <DateRecord />
+  </div>
+</template>
+
+<script setup lang="ts">
+import DateRecord from "./components/DateRecord.vue";
+</script>
+
+<style scoped lang="less">
+.record {
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 20px;
+  min-height: 500px;
+  .record-title {
+    & span:last-child {
+      font-size: 12px;
+      color: @jxColor;
+      border: 1px solid @jxColor;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+  }
+}
+</style>
