d08cd811478b8f43668756bf58b5d64f34565bf2
agreenbird1 <487751965@qq.com>
2022-07-30 14:37:35 +0800

    feat: 添加本地存储，实现持久化

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,11 +1,17 @@
 import { defineStore } from "pinia";
+import storage from "@/utils/storage";
+import { IUser } from "@/api";
 
 export const useUserStore = defineStore({
   id: "user",
-  state: () => ({
-    id: 0,
-    nickname: "",
-    avatar: "",
-  }),
+  state: () => {
+    return (
+      storage.getStorage<IUser>("user") || {
+        id: 0,
+        nickname: "",
+        avatar: "",
+      }
+    );
+  },
   actions: {},
 });
