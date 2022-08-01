3fe5a658aa323b0bad4a2d4943fed35c41ded0c5
afanti <afanti@juexiaotime.com>
2022-08-01 16:09:02 +0800

    feat: 完成全部做题记录页

--- src/views/record/components/DateRecord.vue
+++ src/views/record/components/DateRecord.vue
@@ -2,24 +2,37 @@
   <div class="date-record mb-20">
     <div class="title">
       <img src="../../../assets/icons/date.png" />
-      <span>2022/06/20</span>
+      <span
+        >{{ date.split("-")[0] }}/{{ date.split("-")[1] }}/{{
+          date.split("-")[2]
+        }}</span
+      >
     </div>
     <div class="record-list">
       <record-item
-        v-for="i in 7"
-        :key="i"
-        :total-scores="10"
-        :scores="2"
-        :total-questions="10"
-        :questions="2"
-        chapter-name="刑法的解释"
+        v-for="record in records"
+        :id="record.id"
+        :key="record.id"
+        :total-scores="record.totalScore"
+        :scores="record.getScore"
+        :total-questions="record.totalOtopic"
+        :questions="record.doOtopicNum"
+        :chapter-name="record.content"
         class="mt-20"
       />
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { IRecord } from "@/api";
+
+interface IDateRecord {
+  date: string;
+  records: IRecord[];
+}
+defineProps<IDateRecord>();
+</script>
 
 <style scoped lang="less">
 .date-record {
--- src/views/record/index.vue
+++ src/views/record/index.vue
@@ -1,14 +1,46 @@
 <template>
-  <div class="record mt-20">
+  <div v-if="dateRecords.length" class="record mt-20">
     <main-header title="做题记录"></main-header>
-    <DateRecord />
-    <DateRecord />
-    <DateRecord />
+    <DateRecord
+      v-for="dateRecord in dateRecords"
+      :key="dateRecord.date"
+      :date="dateRecord.date"
+      :records="dateRecord.records"
+      class="mt-20"
+    />
   </div>
+  <empty-loading v-else />
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+import { getRecords, IRecord } from "@/api";
 import DateRecord from "./components/DateRecord.vue";
+
+interface IDateRecord {
+  date: string;
+  records: IRecord[];
+}
+
+const dateRecords = ref<IDateRecord[]>([]);
+getRecords().then((res) => {
+  const temp: Record<string, IRecord[]> = {};
+  res.data.data.forEach((recordItem) => {
+    const key = recordItem.createTime.split("T")[0];
+    if (!temp[key]) temp[key] = [];
+    temp[key].push(recordItem);
+  });
+  for (const iterator in temp) {
+    dateRecords.value.push({
+      date: iterator,
+      records: temp[iterator],
+    });
+  }
+  // 日期当前格式为 2022-07-12 格式，直接去除 - 符号，再用一元运算符转换即可
+  dateRecords.value.sort(
+    (a, b) => +a.date.replaceAll("-", "") - +b.date.replaceAll("-", "")
+  );
+});
 </script>
 
 <style scoped lang="less">
