e6fb8b7f5be88c01812970697e79d798d3fccfbe
agreenbird1 <487751965@qq.com>
2022-08-01 22:36:48 +0800

    feat: 滚动条滚动动画

--- src/utils/scrollTo.ts
+++ src/utils/scrollTo.ts
@@ -1,4 +1,16 @@
+let timer: NodeJS.Timer | null = null;
+
 export default (scrollTop: number) => {
+  clearInterval(timer as NodeJS.Timer);
   const app = document.querySelector("#app")!;
-  const step = scrollTop - app.scrollTop;
+  let step = (scrollTop - app.scrollTop) / 10;
+  // 总是向上取整  所以负数时候就是取floor
+  step = step > 0 ? Math.ceil(step) : Math.floor(step);
+  timer = setInterval(() => {
+    step = (scrollTop - app.scrollTop) / 10;
+    step = step > 0 ? Math.ceil(step) : Math.floor(step);
+    if (Math.abs(app.scrollTop - scrollTop) > 1) {
+      app.scrollTop += step;
+    } else clearInterval(timer as NodeJS.Timer);
+  });
 };
