4b364fb0ddb996b6ff61b91276e196c134d110ec
afanti <afanti@juexiaotime.com>
2022-08-02 15:55:44 +0800

    feat: 引入虚拟列表

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import App from "./App.vue";
 import router from "./router/index";
 import "@/assets/styles/normalize.less";
 import "ant-design-vue/dist/antd.css";
+import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
+import VueVirtualScroller from "vue-virtual-scroller";
 import components from "./components";
 
 const app = createApp(App);
 app.use(store);
+app.use(VueVirtualScroller);
 app.use(router);
 app.use(components);
 app.mount("#app");
