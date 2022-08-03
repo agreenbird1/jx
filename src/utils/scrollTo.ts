78f45786574aea31bb3f669a0d2e95d6934414be
afanti <afanti@juexiaotime.com>
2022-08-03 13:43:33 +0800

    feat: 缓动效果控制

--- src/utils/scrollTo.ts
+++ src/utils/scrollTo.ts
@@ -1,16 +1,22 @@
 let timer: NodeJS.Timer | null = null;
 
-export default (scrollTop: number) => {
+/**
+ * @param scrollTop - 需要滚动到的位置
+ * @param animated - 是否需要缓动效果，不需要则会直接定位到滚动位置
+ */
+export default (scrollTop: number, animated = true) => {
   clearInterval(timer as NodeJS.Timer);
   const app = document.querySelector("#app")!;
-  let step = (scrollTop - app.scrollTop) / 10;
-  // 总是向上取整  所以负数时候就是取floor
-  step = step > 0 ? Math.ceil(step) : Math.floor(step);
-  timer = setInterval(() => {
-    step = (scrollTop - app.scrollTop) / 10;
+  if (animated) {
+    let step = (scrollTop - app.scrollTop) / 10;
+    // 总是向上取整  所以负数时候就是取floor
     step = step > 0 ? Math.ceil(step) : Math.floor(step);
-    if (Math.abs(app.scrollTop - scrollTop) > 1) {
-      app.scrollTop += step;
-    } else clearInterval(timer as NodeJS.Timer);
-  });
+    timer = setInterval(() => {
+      step = (scrollTop - app.scrollTop) / 10;
+      step = step > 0 ? Math.ceil(step) : Math.floor(step);
+      if (Math.abs(app.scrollTop - scrollTop) > 1) {
+        app.scrollTop += step;
+      } else clearInterval(timer as NodeJS.Timer);
+    });
+  } else app.scrollTop = scrollTop;
 };
