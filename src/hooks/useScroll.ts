df17cfd9556154974a49b4f386549d89cc4704eb
afanti <afanti@juexiaotime.com>
2022-08-03 13:41:42 +0800

    feat: 滚动事件hook

---
+++ src/hooks/useScroll.ts
@@ -0,0 +1,33 @@
+import { ref } from "vue";
+import throttle from "@/utils/throttle";
+
+export interface IUseScrollOptions {
+  throttleTime?: number; // scroll 滚动事件的节流控制
+  onScroll?: (e: Event) => void; // 滚动事件时候触发的事件
+}
+
+const useScroll = (
+  element: HTMLElement | SVGElement | Window | Document | null | undefined,
+  options: IUseScrollOptions = {}
+) => {
+  const top = ref(0);
+  const left = ref(0);
+  element ??= document.querySelector("#app") as HTMLElement;
+  element.addEventListener(
+    "scroll",
+    throttle<(e: Event) => void>((e: Event) => {
+      const target = e.target as HTMLElement;
+      const { onScroll } = options;
+      top.value = target.scrollTop;
+      left.value = target.scrollLeft;
+      // 触发滚动回调，如果有的话
+      onScroll && onScroll(e);
+    }, 100)
+  );
+  return {
+    top,
+    left,
+  };
+};
+
+export default useScroll;
